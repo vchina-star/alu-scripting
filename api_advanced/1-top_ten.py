14218850c78cf5e4ce01d8344b471cfe84d47d06
vchina-star <v.china@alustudent.com>
2026-07-22 21:45:21 +0200

    Adjust request for top_ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,11 +5,9 @@ import requests
 
 def top_ten(subreddit):
     """Print the 10 hot post titles, or None if subreddit is invalid."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "python:alu-scripting:v1.0 (by /u/vchina-star)"}
-    params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "ALU-scripting:v1.0 (by /u/vchina-star-alu)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         print(None)
